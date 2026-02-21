e5359f7e1ad7ee847d2ec7688c5de2be6dd60ea2
aryasis87 <aryasis87@gmail.com>
2026-02-21 20:20:58 +0700

    1

--- src/auto-lose-system/auto-lose-system.module.ts
+++ src/auto-lose-system/auto-lose-system.module.ts
@@ -4,9 +4,13 @@ import { Module } from '@nestjs/common';
 import { AutoLoseSystemService } from './auto-lose-system.service';
 import { AutoLoseSystemController } from './auto-lose-system.controller';
 import { FirebaseModule } from '../firebase/firebase.module';
+import { AuthModule } from '../auth/auth.module'; 
 
 @Module({
-  imports: [FirebaseModule],
+  imports: [
+    FirebaseModule,
+    AuthModule, 
+  ],
   controllers: [AutoLoseSystemController],
   providers: [AutoLoseSystemService],
   exports: [AutoLoseSystemService],
