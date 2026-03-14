739cbf3fc0a1216b3695c5a509d23b2fe29cdec4
aryasis87 <aryasis87@gmail.com>
2026-03-14 09:12:12 +0700

    1

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,6 @@
+import * as dns from 'dns';
+dns.setDefaultResultOrder('ipv4first');
+
 // src/main.ts
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe, Logger } from '@nestjs/common';
